fe566cea24b135deab95e38b1898161c7a1ab59f
Kallif_Abrahao <kallif.abrahao@comerc.com.br>
2025-05-12 11:15:28 -0300

    removendo arquivo

--- src/dataTable.ts
+++
@@ -1,7 +0,0 @@
-import { applyPureVueInReact } from "veaury";
-import { CeDataTable } from "@comerc-teste/vue-components";
-import type { FC } from "react";
-
-const DataTable = applyPureVueInReact(CeDataTable);
-
-export default DataTable;
